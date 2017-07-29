045e03a1f45d3c9fd57dc3ccbadf1f0f3dd84d96
Andrew Eells <andrew.eells3@gmail.com>
2017-07-29 15:06:09 -0700

    starting cronjob

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-export SLACK_SECRET="xoxb-215088501767-HbdmFMsiiRz5N1b5dLsWPXNy";
+export SLACK_SECRET="xoxb-214896941286-GuK4R4CCgseCVvViQFin3Y8T";
 export MONGODB_URI="mongodb://user:user@ds139370.mlab.com:39370/bot_practice";
 export GOOGLE_CLIENT_ID="727893931271-lererr8k08ac38p53ptjc40n5e880h0m.apps.googleusercontent.com";
 export GOOGLE_CLIENT_SECRET="FDiqMLCuJSow8_-WflYCnMeL";
