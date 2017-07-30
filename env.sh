3d6c62f0bbc0964e437da7452b1a1057d3191d37
yashnevatia <yash1997@hku.hk>
2017-07-29 17:40:31 -0700

    Delete env.sh

--- env.sh
+++
@@ -1,5 +0,0 @@
-export SLACK_SECRET="xoxb-214896941286-GuK4R4CCgseCVvViQFin3Y8T";
-export MONGODB_URI="mongodb://user:user@ds139370.mlab.com:39370/bot_practice";
-export GOOGLE_CLIENT_ID="727893931271-lererr8k08ac38p53ptjc40n5e880h0m.apps.googleusercontent.com";
-export GOOGLE_CLIENT_SECRET="FDiqMLCuJSow8_-WflYCnMeL";
-export DOMAIN="http://localhost:3000";
